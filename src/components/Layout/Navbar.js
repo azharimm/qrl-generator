17e687e9bab3f5456f2a1f7924eac30014494341
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-29 14:58:48 +0700

    ^ navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,42 +1,37 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import CropFreeIcon from '@material-ui/icons/CropFree';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
-    title: {
-        flexGrow: 1
-    }
-}));
 
 const Navbar = () => {
-    const classes = useStyles();
     return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <CropFreeIcon style={{marginRight: 5}} />
-                    <Typography variant="h6" className={classes.title}>
-                        Qrl Shortener
-                    </Typography>
-                    <Button color="inherit">
-                        Home
-                    </Button>
-                    <Button color="inherit">
-                        History
-                    </Button>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">
+                QRL Generator
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            History
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
