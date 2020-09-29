ba0dff3eb2b4271bfc491d63f9a242062088cbcf
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-29 15:21:48 +0700

    ^ history page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Home from './pages/Home'
-// import './App.css';
+import History from './pages/History'
 
 const App = () => {
     return (
         <div className="App">
-            <Navbar />
-            <Home />
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/history">
+                        <History />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 };
